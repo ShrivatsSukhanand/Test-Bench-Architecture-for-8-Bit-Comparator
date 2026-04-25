6438118a3fd5a76eff7be5ffa8bc740176feae8c
shrivats <antediluvianmono@gmail.com>
2026-04-25 19:54:57 +0530

    Create generator.sv

---
+++ generator.sv
@@ -0,0 +1,23 @@
+class generator;
+//generator creates transaction and puts it into mailbox
+transaction tr;
+mailbox mbox;
+//constructor
+function new(mailbox mbox);
+this.mbox = mbox;
+endfunction
+//task for putting tr into mailbox
+task run();
+tr=new();
+repeat(10)
+begin
+tr.randomize();
+if(!tr.randomize())
+$display("Randomization has failed");
+$display("a = %d, b = %d ",tr.a,tr.b); //display generated values
+//put tr into mailbox
+mbox.put(tr);
+#10;
+end
+endtask
+endclass
