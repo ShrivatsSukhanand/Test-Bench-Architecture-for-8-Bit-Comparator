130a341e6ee06264a4231365e73c63ecbea50fd3
shrivats <antediluvianmono@gmail.com>
2026-04-29 00:25:04 +0530

    Update driver.sv
    used non-blocking assignments

--- driver.sv
+++ driver.sv
@@ -1,26 +1,30 @@
 class driver;
-//driver takes tr from mail box and puts it into interface wires
-virtual comp_intr vif;
-mailbox mbox;
-transaction tr;
-//constructor
-function new( virtual comp_intr vif, mailbox mbox);
-begin
-this.vif = vif;
-this.mbox = mbox;
-4
-tr=new();
-end
-endfunction
-task run();
-forever
-begin
-mbox.get(tr);
-vif.a=tr.a;
-vif.b=tr.b;
-#5 //delay for output to be generated
-tr.result=vif.result;
-$display("result = %d",tr.result);
-end
-endtask
+  
+  //driver takes tr from mail box and puts it into interface wires
+  
+   virtual comp_intr vif;
+   mailbox mbox;
+   transaction tr;
+  int i = 1;
+  //constructor
+  function new( virtual comp_intr vif, mailbox mbox);
+     begin
+       this.vif = vif;
+       this.mbox = mbox;
+       tr=new();
+     end
+  endfunction
+  
+  task run();
+    forever
+      begin
+        mbox.get(tr);
+        vif.a<=tr.a;
+        vif.b<=tr.b;
+        #5; //delay for output to be generated
+       // $display("[%d,%t] DRIVER",i++,$time);
+      end
+  endtask
+  
+  
 endclass
