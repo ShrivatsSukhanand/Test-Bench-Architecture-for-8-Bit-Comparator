6b2c2420bf1b663726049738692b3f0bbf0b8624
shrivats <antediluvianmono@gmail.com>
2026-04-29 00:29:16 +0530

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,18 +1,20 @@
-//top module
-‘include "interface.sv"
-‘include "test.sv"
+`include "interface.sv"
+`include "test.sv"
+
 module testbench1;
-comp_intr rif();
-test tb(rif);
-comp mymodule(
-.a(rif.a),
-.b(rif.b),
-.result(rif.result)
-);
-initial
-begin
-$dumpfile("dump.vcd");
-$dumpvars(0,testbench1);
-#100;
-end
+  
+  comp_intr rif();
+  test tb(rif);
+  comp mymodule(
+    .a(rif.a),
+    .b(rif.b),
+    .result(rif.result)
+  );
+    
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars(0,testbench1);
+    end
+  
 endmodule
