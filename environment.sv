02ca967a70b5a8da0200be1dfda5aa5c05db0c2c
shrivats <antediluvianmono@gmail.com>
2026-04-29 00:27:19 +0530

    Update environment.sv
    added instances of monitor and scoreboard

--- environment.sv
+++ environment.sv
@@ -1,23 +1,37 @@
-‘include "transaction.sv"
-‘include "generator.sv"
-‘include "driver.sv"
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+
 class environment;
-generator gen;
-driver drv;
-mailbox mbox;
-virtual comp_intr vif;
-function new(virtual comp_intr vif);
-begin
-this.vif=vif;
-mbox=new();
-gen=new(mbox);
-drv=new(vif, mbox);
-end
-endfunction
-task run();
-fork
-gen.run();
-drv.run();
-join
-endtask
-endclass
+  generator gen;
+  driver drv;
+  monitor mon;
+  scoreboard scr;
+  mailbox mbox;
+  mailbox m;
+  virtual comp_intr vif;
+  
+  function new(virtual comp_intr vif);
+    begin
+     this.vif=vif;
+     mbox=new();
+     m=new();
+     gen=new(mbox);
+     drv=new(vif, mbox);
+     mon=new(vif,m);
+     scr=new(m);
+    end
+  endfunction
+  
+  task run();
+    fork
+      gen.run();
+      drv.run();
+      mon.run();
+      scr.run();
+    join_any
+  endtask
+  
+endclass 
